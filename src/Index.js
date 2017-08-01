79c502b3b56d690bd7fbfd5ac59645a7488647f5
Sochima Nwobia <sochimadn@gmail.com>
2017-08-01 13:09:14 +0100

    Fixed Index.js

--- src/Index.js
+++ src/Index.js
@@ -3,22 +3,23 @@ const fs = require('fs');
 const quest = require('request');
 const cheerio = require('cheerio');
 var jsonfile = require('jsonfile');
-
-
+var pageURL;
+//people can specify user
 
 
 module.exports = {
 
-    get: function get() {
-
-        var pageURL = (channel + user);
+    get: function get(user) {
+        var puser = user;
 
         function scrapePage() {
+            pageURL = channel + puser;
             //make an HTTP request for the page to be scraped
             request(pageURL, function(error, response, responseHtml) {
 
                 //write the entire scraped page to the local file system
-                info = JSON.stringify(responseHtml);
+                JSON.stringify(responseHtml);
+                //console.log
             });
         }
 
